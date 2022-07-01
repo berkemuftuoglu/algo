21a396a165af76dc0a937093cf6b52756ea635bc
Berke Muftuoglu <berke.muftuoglu@getir.com>
2022-07-01 12:35:12 +0100

    functions

---
+++ usefulCppFuncs/builtIn.cpp
@@ -0,0 +1,31 @@
+
+/*
+Built-in useful functions for interview questions
+
+Author: Berke Muftuoglu
+*/
+#include <algorithm>
+
+
+using namespace std;
+
+class Functions{
+public:
+    int main(){ }
+
+/*
+SWAP()
+*/
+void swapExample(){
+    int a = 5, b = 10;
+    swap(a,b);
+}
+
+
+
+
+
+    
+
+};
+
---
+++ usefulCppFuncs/notBuiltIn.cpp
@@ -0,0 +1,21 @@
+/*
+COUNT()
+count the numbers in an array
+*/
+#include <algorithm>
+
+class Functions{
+public:
+
+int sizeOfAnArray(int *arr){
+    size_t res = sizeof(arr)/sizeof(*arr);
+    return res;
+
+    // or #define sizeArr(arr) sizeof(arr)/sizeof(*arr)
+}
+
+
+
+
+
+};
