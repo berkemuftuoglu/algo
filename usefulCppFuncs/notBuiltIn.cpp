d4519680b2d827d8efac545d9dd20698b58bf06d
Berke Muftuoglu <berkemuftuoglu1@gmail.com>
2022-07-05 11:41:58 +0100

    added new functions

--- usefulCppFuncs/notBuiltIn.cpp
+++ usefulCppFuncs/notBuiltIn.cpp
@@ -3,10 +3,15 @@ COUNT()
 count the numbers in an array
 */
 #include <algorithm>
+#include <vector>
+#include <iostream>
+#include <set>
+using namespace std;
 
 class Functions{
 public:
 
+//size of an array
 int sizeOfAnArray(int *arr){
     size_t res = sizeof(arr)/sizeof(*arr);
     return res;
@@ -14,8 +19,26 @@ int sizeOfAnArray(int *arr){
     // or #define sizeArr(arr) sizeof(arr)/sizeof(*arr)
 }
 
+// size of an array
+bool containsDuplicate(vector<int>& a) {
+        set<int> s(a.begin(), a.end());
+        return s.size() != a.size();
+    }
 
-
+// move all occurrences of n to the end of the array
+void moveElemToEnd(vector<int>& nums, int n) {
+    int j = 0;
+    // move all the nonzero elements advance
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] != n) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }
+    for (;j < nums.size(); j++) {
+            nums[j] = n;
+    }
+}
 
 
 };
